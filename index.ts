aa7430556deafefae5e96cc63c42dbb9252514b0
Lukasz Wilisowski <l.wilisowski@optimumpareto.com>
2023-01-05 10:25:50 +0100

    npm test

---
+++ index.ts
@@ -0,0 +1,15 @@
+//
+export { MongoDbRepository } from './src/db/mongoose/mongo.repository';
+export {
+  IDomainRepository,
+  IReadDomainRepository,
+  IWriteDomainRepository
+} from './src/interfaces/repository.interface';
+export { SearchBy } from './src/interfaces/search/search.by.interface';
+export { SearchCriteria } from './src/interfaces/search/search.criteria.interface';
+export { UpdateCriteria } from './src/interfaces/update/update.criteria.interface';
+export { UpdateWith } from './src/interfaces/update/update.with.interface';
+export { MockedDBRepository } from './src/mocked-repository/mocked.repository';
+export { MapTo } from './src/object-entity-mapper/interfaces/map.to.interface';
+export { Mapping } from './src/object-entity-mapper/interfaces/mapping.interface';
+export { ObjectEntityMapper } from './src/object-entity-mapper/object.entity.mapper';
