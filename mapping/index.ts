0637ec957293cf33dddedab68950030b354e7145
Lukasz Wilisowski <wilisowskilukasz@gmail.com>
2024-01-02 14:49:58 +0100

    exported transform types

--- mapping/index.ts
+++ mapping/index.ts
@@ -1,2 +1,8 @@
 export { MapTo } from '../src/object-entity-mapper/helpers/map.to.helper';
 export { Mapping } from '../src/object-entity-mapper/interfaces/mapping.interface';
+export {
+  TransformArray,
+  TransformArrayOfObjects,
+  TransformNestedObject,
+  TransformProperty
+} from '../src/object-entity-mapper/interfaces/mapping.transforms';
