1099b99871a998ba1d1ec364ce094b330ea7775e
Lukasz Wilisowski <lukasz.wilisowski@willcodeacademy.com>
2024-08-19 10:29:01 +0200

    exported types

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ export { SingleEntityNotFoundError } from './src/errors/singleEntityNotFound.err
 export { SearchBy } from './src/helpers/search.by.helper';
 export { UpdateWith } from './src/helpers/update.with.helper';
 export {
-  IDomainRepository,
-  IReadDomainRepository,
-  IWriteDomainRepository
+  type IDomainRepository,
+  type IReadDomainRepository,
+  type IWriteDomainRepository
 } from './src/interfaces/repository.interface';
 export { SearchCriteria } from './src/interfaces/search/search.criteria.interface';
 export { UpdateCriteria } from './src/interfaces/update/update.criteria.interface';
